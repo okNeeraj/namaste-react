c46d587a2be661854fbdb7a8c1af6184fd1c60d0
Neeraj Singh <neerajonetwo@gmail.com>
2023-09-10 10:10:21 +0530

    Episod 11 - HOC improved

--- src/components/hoc/HocComponent.js
+++ src/components/hoc/HocComponent.js
@@ -2,11 +2,10 @@ import { useState } from 'react';
 import Card, { HocCard } from "./Card";
 
 const HocComponent = () => {
-	const [isAuthenticate, setIsAuthenticate] = useState(true);
+	const [isAuthenticate, setIsAuthenticate] = useState(false);
 	const MyHocCard = HocCard(Card);
-	return (
-		isAuthenticate ? <MyHocCard /> : <Card />
-	)
+
+	return isAuthenticate ? <MyHocCard /> : <Card />
 }
 
 export default HocComponent;
